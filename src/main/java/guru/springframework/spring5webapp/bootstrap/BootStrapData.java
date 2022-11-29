fbbcc0edfa237990fd2aebba9dcc9e161bd6cd8a
Stere <scaciandone@ptc.com>
2022-11-29 13:56:42 +0200

    Task3

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -26,31 +26,40 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String ... args) throws Exception {
 
+    Publisher pb = new Publisher("Bd Unirii, 32" , "Bucharest","Romania", "31312", "TestPublisher");
+    publisherRepository.save(pb);
+    System.out.println("No of publishers : " + publisherRepository.count());
+
     Author eric = new Author("Steve", "Evans");
     Book book = new Book("Domain driven design","12312312312");
 
     eric.getBooks().add(book);
     book.getAuthors().add(eric);
+    book.setPublisher(pb);
+    pb.getBooks().add(book);
 
     authorRepository.save(eric);
     bookRepository.save(book);
+    publisherRepository.save(pb);
 
     Author rod = new Author("Rod", "Evans");
     Book noEJB = new Book("J2EE Development without EJB","323213123");
 
     rod.getBooks().add(noEJB);
     noEJB.getAuthors().add(rod);
+    noEJB.setPublisher(pb);
 
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
+    authorRepository.save(rod);
+    bookRepository.save(noEJB);
 
-    System.out.println(" Started in Bootstrap");
-    System.out.println("No of Books : " + bookRepository.count());
-
-    Publisher pb = new Publisher("Bd Unirii, 32" , "Bucharest","Romania", "31312", "TestPublisher");
+    pb.getBooks().add(noEJB);
     publisherRepository.save(pb);
 
-        System.out.println("No of publishers : " + publisherRepository.count());
+    System.out.println("Started in Bootstrap");
+    System.out.println("No of Books : " + bookRepository.count());
+
+    System.out.println("Publisher number of books " + pb.getBooks().size());
+
 
 
     }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -15,6 +17,10 @@ public class Publisher {
     private String state;
     private String zipCode;
 
+    @OneToMany
+    @JoinColumn(name="publisher_id")
+    private Set<Book> books = new HashSet<>();
+
     public Publisher() {
     }
 
@@ -24,6 +30,14 @@ public class Publisher {
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public Long getId() {
